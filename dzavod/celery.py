af027de7cf735ada951834a2eaf1ee6f699c1933
mayldute <mayldute@icloud.com>
2025-07-08 09:22:20 +0200

    celery update

--- dzavod/celery.py
+++ dzavod/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dzavod.settings')
 
 app = Celery('dzavod')
 app.config_from_object('django.conf:settings', namespace='CELERY')
