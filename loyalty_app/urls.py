a42b1a54450e47c94f5c9456085cd33654c9d8b4
mayldute <mayldute@icloud.com>
2025-07-08 18:45:12 +0200

    promotion

--- loyalty_app/urls.py
+++ loyalty_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PointsTransactionViewSet, LoyaltyCardViewSet
+from .views import PointsTransactionViewSet, LoyaltyCardViewSet, PromotionViewSet
 
 loyalty_app_router = routers.DefaultRouter()
 # loyalty_app_router.register(r'loyalty-cards', LoyaltyCardViewSet, basename='loyaltycard')
 loyalty_app_router.register(r'points-transactions', PointsTransactionViewSet, basename='points-transactions')
+loyalty_app_router.register(r'promotions', PromotionViewSet, basename='promotions')
 
 
 urlpatterns = [
