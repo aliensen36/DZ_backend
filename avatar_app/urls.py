fa06d02077c4c7c93b43f1cd0ad5cb5243f79e37
mayldute <mayldute@icloud.com>
2025-07-18 10:23:51 +0200

    update avatar urls

--- avatar_app/urls.py
+++ avatar_app/urls.py
@@ -3,10 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from .views import AvatarViewSet
+from .views import AvatarViewSet, UserAvatarProgressViewSet
 
 avatar_app_router = routers.DefaultRouter()
 avatar_app_router.register(r'avatars', AvatarViewSet, basename='avatar')
+avatar_app_router.register(r'avatars', UserAvatarProgressViewSet, basename='avatar-activate')
 
 urlpatterns = [
     path('', include(avatar_app_router.urls)),
