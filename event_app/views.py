6065030962efd6c9d6947a9bb518a4dc9826d83b
mayldute <mayldute@icloud.com>
2025-07-18 09:55:59 +0200

    update get events

--- event_app/views.py
+++ event_app/views.py
@@ -1,5 +1,4 @@
-from datetime import timezone
-
+from django.utils import timezone
 from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
