9a9d010c66c256618f258ef3995c29ccbe9a109e
mayldute <mayldute@icloud.com>
2025-08-08 15:34:34 +0300

    update event model

--- event_app/models.py
+++ event_app/models.py
@@ -5,19 +5,19 @@ from django.db import models
 
 class Event(models.Model):
     """Модель для представления мероприятий."""
-    title = models.CharField(max_length=255, unique=True, verbose_name='Название')
-    description = models.TextField(max_length=255, verbose_name='Описание')
-    info = models.TextField(verbose_name='Дополнительная информация')
+    title = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    description = models.TextField(max_length=100, verbose_name='Описание')
+    info = models.TextField(max_length=400, verbose_name='Дополнительная информация')
     start_date = models.DateTimeField(verbose_name='Дата начала')
     end_date = models.DateTimeField(verbose_name='Дата окончания')
-    location = models.CharField(max_length=255, verbose_name='Место проведения')
+    location = models.CharField(max_length=100, verbose_name='Место проведения')
     photo = models.ImageField(upload_to='events/photos/', verbose_name='Фото')
     # Флажки
     enable_registration = models.BooleanField("Доступна регистрация", default=False)
     enable_tickets = models.BooleanField("Доступна покупка билетов", default=False)
     # Ссылки (необязательные, если флажки выключены)
-    registration_url = models.URLField(null=True, blank=True, verbose_name='Ссылка на регистрацию')
-    ticket_url = models.URLField(null=True, blank=True, verbose_name='Ссылка на покупку билета')
+    registration_url = models.URLField(max_length=70, null=True, blank=True, verbose_name='Ссылка на регистрацию')
+    ticket_url = models.URLField(max_length=70, null=True, blank=True, verbose_name='Ссылка на покупку билета')
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
