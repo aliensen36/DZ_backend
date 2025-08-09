ee2c3cc270f548b955abe7ed7043d32c33643375
mayldute <mayldute@icloud.com>
2025-08-09 23:04:31 +0300

    update point transaction web-admin

--- loyalty_app/forms.py
+++ loyalty_app/forms.py
@@ -36,6 +36,11 @@ class PointsTransactionForm(forms.ModelForm):
         model = PointsTransaction
         fields = ['price', 'transaction_type', 'card_id', 'resident_id']
 
+    class PointsTransactionForm(forms.ModelForm):
+    class Meta:
+        model = PointsTransaction
+        fields = ['price', 'transaction_type', 'card_id', 'resident_id']
+
     def clean(self):
         cleaned_data = super().clean()
         price = cleaned_data.get('price')
@@ -46,21 +51,24 @@ class PointsTransactionForm(forms.ModelForm):
         if price is not None and price <= 0:
             raise ValidationError({'price': 'Сумма должна быть положительной.'})
 
+        settings = PointsSystemSettings.objects.first()
+        if not settings:
+            raise ValidationError('Не заданы настройки программы лояльности.')
+
         if transaction_type == 'начисление':
             if not resident:
                 raise ValidationError({'resident_id': 'Нужно выбрать резидента для расчета баллов.'})
-            points = int((price / 100) * resident.points_per_100_rubles)
+
+            points = int((price / 100) * settings.points_per_100_rubles)
             if points <= 0:
                 raise ValidationError({'price': 'Недостаточно суммы для начисления баллов.'})
             cleaned_data['points'] = points
 
         elif transaction_type == 'списание':
-            if not resident:
-                raise ValidationError({'resident_id': 'Нужно выбрать резидента для расчета лимита списания.'})
             if not card:
                 raise ValidationError({'card_id': 'Нужно выбрать карту.'})
 
-            max_percent = resident.max_deduct_percent / 100
+            max_percent = settings.points_per_1_percent / 100
             max_deductible_points = int(price * max_percent)
             current_balance = card.get_balance()
 
@@ -71,11 +79,11 @@ class PointsTransactionForm(forms.ModelForm):
                     'card_id': f'Недостаточно баллов. Баланс: {current_balance}, требуется: {max_deductible_points}'
                 })
             cleaned_data['points'] = -max_deductible_points
+
         else:
             raise ValidationError({'transaction_type': 'Недопустимый тип транзакции.'})
 
         return cleaned_data
-
 
 
 class PromotionAdminForm(forms.ModelForm):
